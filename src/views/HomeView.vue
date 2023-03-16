7268054e1c0dd94933143ce8a9e69e84475ddd0d
TheFahim <abdul15-980@diu.edu.bd>
2023-03-16 18:39:43 +0600

    Two mental Leaps to Script Setup" ; :

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/TheWelcome.vue';
+
+
+  // eslint-disable-next-line no-undef
+  let message = $ref("Hello world");
+
+  setTimeout(() => {
+    message = "I have bee changed";
+  }, 2000);
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+   <p> {{ message }}</p>
+   <p>
+    <input type="text" v-model="message">
+   </p>
+
   </main>
 </template>
